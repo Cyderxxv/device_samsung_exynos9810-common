c8996992b5c6040a98725d6af6fb65a1a2c34199
Dylan Neve <dylanneve1@gmail.com>
2022-05-23 22:03:59 +0100

    exynos9810-common: Revert to stock thermal HAL
    * Errors with current source built HAL

--- common.mk
+++ common.mk
@@ -295,10 +295,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.samsung
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # Touch HAL
 PRODUCT_PACKAGES += \
